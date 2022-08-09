3694906e3c837b448b154bd4d1f6d70f93bf7b94
Spencer Krull <spencerkrull138@gmail.com>
2022-08-08 22:55:36 -0600

    finished controllers and routes from activity

---
+++ config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "music_tour",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "music_tour_test",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "production": {
+    "username": "postgres",
+    "password": "postgres",
+    "database": "music_tour_production",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  }
+}
